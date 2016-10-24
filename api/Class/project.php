374cac81675f3151b1e4f2627bd2f2a84cdf81fe
SimonLoir <simon-loir@hotmail.com>
2016-10-24 17:40:15 +0200

    Modif des commantaires dans API/project.php

--- api/Class/project.php
+++ api/Class/project.php
@@ -1,7 +1,7 @@
 <?php 
 Class project{
 	/*
-	PRE :
+	VAR :
 		* id (int)
 		* name (string)
 		* managers (string)
@@ -12,24 +12,18 @@ Class project{
 		* shortDescription (string)
 		* goals (string)
 		* links (string)
-
-	POST :
-
-		* id (int)
-		* name (string)
-		=> * managers (array)
-		* type (string)
-		* progression (int)
-		* pined (int)
-		* description (string)
-		* shortDescription (string)
-		* goals (string)
-		* links (string)
-
-		|| UError (user error)
 	*/
+	
 	public function clientFormat()
 	{
+		/*
+			PRE :
+				* managers (string) init [comma separated numbers]
+			POST :
+				* managers is now an array [same numbers]
+				IF ?manager = "true" AND user_id isn't in managers : return "UError"
+		*/
+
 		$this->managers = explode(';', $this->managers);
 
 		if ($_GET['manager']  == "true" && !in_array($_SESSION['user_id'], $this->managers)) {
--- api/Class/projects.php
+++ api/Class/projects.php
@@ -28,7 +28,7 @@ Class projects{
 
 		$this->managers = explode(';', $this->managers);
 
-		if (in_array($_SESSION['user_id'], $this->managers)) {
+		if (isset($_SESSION['user_id']) && in_array($_SESSION['user_id'], $this->managers)) {
 
 			$this->user_is_manager = true;
 

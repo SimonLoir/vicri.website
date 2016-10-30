6ba63ff5c95d3e2fae36f5dfe50c62918c544c4d
SimonLoir <simon-loir@hotmail.com>
2016-10-30 18:45:59 +0100

    Ajout d'espaces pour une meilleure lisibilité

--- api/index.php
+++ api/index.php
@@ -4,9 +4,13 @@ session_start();
 
 //On récupère la méthode
 if (isset($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'])) {
+
 	$method = $_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'];
+
 }else{
+
 	$method = $_SERVER['REQUEST_METHOD'];
+	
 }
 
 
@@ -27,15 +31,10 @@ if ($res != "doc" && $res != "indev") {
 /*
 Initialisation de l'autoloader
 */
+
 include 'Class/autoloader.php';
 spl_autoload_register(["autoloader", "load"]);
 
-/*
-Création de l'objet db 
-
-	=> la connection à la BDD n'est initialisée QUE si on utilise la méthode ->query
-
-*/
 $db = new db($db__base, $db__user , $db__pass, $db__host);
 
 /* 
@@ -44,26 +43,18 @@ $db = new db($db__base, $db__user , $db__pass, $db__host);
 if ($method == "GET") {
 	if ($res == "projects") { 
 
-		// Résulat final
 		$result = [];
 
-		// Requête SQL 
-		/*
-		POST : 
-			[
-				[projet](array),
-				[projet](array),
-				...(array)
-			](array)
-		*/
 		$projects = $db->query('SELECT id, name, managers, shortDescription, progression, pined FROM projects', ['class'=> "projects"]);
 		
 		foreach ($projects as $project) {
+
 			$result[] = $project->clientFormat();
+
 		}
 
-		 exit(json_encode($result)); // return [{projet}, {projet}]
-		//exit(json_encode(["test"]));
+		 exit(json_encode($result));
+
 	}elseif ($res == "videos") { 
 
 		$result = [];
@@ -71,7 +62,9 @@ if ($method == "GET") {
 		$videos = $db->query('SELECT * FROM videos', ['class'=> "videos"]);
 		
 		foreach ($videos as $video) {
+
 			$result[] = $video->clientFormat();
+
 		}
 
 		exit(json_encode($result));
@@ -88,29 +81,44 @@ if ($method == "GET") {
 
 
 	}elseif ($res == "doc") {
+
 		include 'docu.php';
+
 	}elseif ($res == "user_connection_state"){
 
 		if (isset($_SESSION["user_id"])) {
+
 			exit(json_encode($db->query('SELECT name, firstname, mail, pseudo FROM users WHERE id = :user_id', [
+
 				"prepare" => [":user_id" => $_SESSION['user_id']] 
+
 			])));
+
 		}else{
+
 			exit("Empty");
+
 		}
 
 	}elseif ($res == "indev"){
+
 		exit("Dev area");
+
 	}elseif ($res == "logout"){
+
 		session_destroy();
+
 		header('Location: ../');
+
 	}
 
 }if ($method == "POST") {
 	if ($res == "admin::newuser") {
 
 		if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] == 1) {
+
 			if (isset($_POST['name'])&& isset($_POST['firstname'])&& isset($_POST['email'])&& isset($_POST['pseudo'])&& isset($_POST['password'])) {
+
 				if (!empty($_POST['name'])&& !empty($_POST['firstname'])&& !empty($_POST['email'])&& !empty($_POST['pseudo'])&& !empty($_POST['password'])) {
 					
 					$name = $_POST['name'];
@@ -137,9 +145,11 @@ if ($method == "GET") {
 				}else{
 					exit("e");
 				}
+
 			}else{
 				exit("Erreur");
 			}
+
 		}else{
 			exit('Error : you must be connected');
 		}
@@ -147,11 +157,15 @@ if ($method == "GET") {
 	}elseif($res == "login"){
 
 		if (!isset($_POST['email']) || !isset($_POST['password'])) {
+
 			exit(json_encode("Error : the server can't execute that request !"));
+
 		}
 
 		$user_infos = $db->query('SELECT * FROM users WHERE mail = :email', [
+
 			"prepare" => [":email" => $_POST['email']]
+
 		]);
 
 		if ($user_infos != null) {
@@ -163,11 +177,15 @@ if ($method == "GET") {
 				exit(json_encode("ok"));
 
 			}else{
+
 				exit(json_encode("Error : incorrect password"));
+
 			}
 
 		}else{
+
 			exit(json_encode("Error : can't find the user"));
+
 		}
 		
 	}

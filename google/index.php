0ae79f5b508116e4c0fafa658c5eff4aaffda69c
SimonLoir <simon-loir@hotmail.com>
2016-11-20 19:53:52 +0100

    Google Sign In : auth avec le compte google : #test 1

--- google/index.php
+++ google/index.php
@@ -3,6 +3,10 @@ require 'vendor/autoload.php';
 
 session_start();
 
+if (isset($_SESSION['user_id'])) {
+  exit("déjà connecté"); 
+}
+
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret.json');
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'].'/vicri/google');
@@ -54,7 +58,29 @@ if (isset($authUrl)){
 	$user_email = $token_data["email"];
 
   if (strpos($user_email, "@indse.be")) {
-    
+    include '../api/Class/db.php';
+    /* --- --- --- --- --- --- --- --- --- --- */
+    /*  => */include "../../config.php"; /* <= */
+    /* --- --- --- --- --- --- --- --- --- --- */
+    $db = new db($db__base, $db__user , $db__pass, $db__host);
+
+    $user_infos = $db->query('SELECT * FROM users WHERE mail = :email', [
+
+      "prepare" => [":email" => $user_email]
+
+    ]);
+
+    if ($user_infos != null) {
+
+        $_SESSION['user_id'] = $user_infos[0]->id;
+
+        header("Location: ../");
+
+    }else{
+
+      exit(json_encode("L'utilisateur est introuvable"));
+
+    }
   }else{
     echo "Désolé, cette adresse email n'est pas valide";
   }

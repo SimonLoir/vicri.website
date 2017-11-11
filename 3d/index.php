ed4ae049d3618cb42d56bb3f9cdef2d6fa10f908
SimonLoir <simon-loir@hotmail.com>
2017-11-11 20:33:34 +0100

    ajout d'un loader d'objets 3D "url_base"

--- 3d/index.php
+++ 3d/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>vicri object viewer</title>
     <script src="three.js"></script>
 
     <script src="js/loaders/ColladaLoader.js"></script>
@@ -80,7 +80,7 @@
             // collada
 
             var loader = new THREE.ColladaLoader(loadingManager);
-            loader.load('model.dae', function (collada) {
+            loader.load('<?= (isset($_GET["model"]) ? $_GET["model"] : "") ?>', function (collada) {
 
                 object = collada.scene;
 
